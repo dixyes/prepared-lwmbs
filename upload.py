22eabe3509466a20f1b6cedb304775b461e2fd90
dixyes <dixyes@gmail.com>
2023-08-13 16:10:12 +0800

    Disable zchunk

--- upload.py
+++ upload.py
@@ -9,8 +9,8 @@ if os.environ.get("CI"):
     EPEL_MIRROR = "https://mirror.facebook.net/fedora/epel/"
     SCLO_MIRROR = "https://mirror.facebook.net/centos/7/sclo/"
     GNU_MIRROR = "https://mirror.facebook.net/gnu/"
-    # FEDORA_MIRROR = "https://mirror.facebook.net/fedora/linux/"
-    FEDORA_MIRROR = ""
+    FEDORA_MIRROR = "https://mirror.facebook.net/fedora/linux/"
+    # FEDORA_MIRROR = ""
     CENTOS_ALTARCH_MIRROR = "https://mirror.facebook.net/centos-altarch/"
 else:
     CENTOS_MIRROR = "https://mirrors.ustc.edu.cn/centos/"
